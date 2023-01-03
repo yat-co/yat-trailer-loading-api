7b570f96017b25f5769e7ed2f14c2a5885de1045
jvhart <jarodvhart@gmail.com>
2023-01-03 15:56:12 -0600

    [YTL-API-1]  Cleanup and docs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, make_response
 from flask_cors import CORS
 
-from ytl import optimize_trailer_load_plan_wrapper
-import argparse
 from decouple import config
 import logging
 from typing import Dict
+
+from ytl import optimize_trailer_load_plan_wrapper
+from ytl.optimizer_functions import PIECE_ARRANGEMENT_ROUTER, SHIPMENT_ARRANGEMENT_ROUTER
 from ytl.standard_logistic_dims import STANDARD_TRAILER_DIMS
 from ytl.options import PACKING_OPTIONS
 
@@ -24,35 +25,24 @@ consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
-# Create the parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--force-db-fetch', action='store_true')
-args = parser.parse_args()
-
-
 def json_response(data='', status: int = 200, headers: Dict = None):
     headers = headers or {}
     if 'Content-Type' not in headers:
         headers['Content-Type'] = 'application/json'
-    
     return make_response(data, status, headers)
-
-
-allowed_params = [
-    'trailer_dims', 'equipment_code', 'shipment_list', 'allow_rotations', 
-]
 
 @app.route('/api/ytl/optimize-trailer-load/', methods=['POST'])
 def trailer_load_optimize():
     if request.method == 'POST':
         status_code,response = optimize_trailer_load_plan_wrapper(request.json)
         return json_response(data=response, status=status_code)
-
+    return json_response(data={'error' : f'Request type {request.method} not accepted'}, status=400)
 
 @app.route('/api/ytl/equipment-options/', methods=['GET'])
 def trailer_load_equipment_options():
     if request.method == 'GET':
         return json_response(data=STANDARD_TRAILER_DIMS, status=200)
+    return json_response(data={'error' : f'Request type {request.method} not accepted'}, status=400)
 
 
 @app.route('/api/ytl/packing-type-options/', methods=['GET'])
@@ -65,6 +55,21 @@ def trailer_load_packing_options():
             } for a,b in PACKING_OPTIONS
         ]
         return json_response(data=res, status=200)
+    return json_response(data={'error' : f'Request type {request.method} not accepted'}, status=400)
+
+@app.route('/api/ytl/piece-arrangement-algorithm-options/', methods=['GET'])
+def trailer_load_piece_arrangement_algorithm_options():
+    if request.method == 'GET':
+        res = [b for a,b in PIECE_ARRANGEMENT_ROUTER.values()]
+        return json_response(data=res, status=200)
+    return json_response(data={'error' : f'Request type {request.method} not accepted'}, status=400)
+
+@app.route('/api/ytl/shipment-arrangement-algorithm-options/', methods=['GET'])
+def trailer_load_shipment_arrangement_algorithm_options():
+    if request.method == 'GET':
+        res = [b for a,b in SHIPMENT_ARRANGEMENT_ROUTER.values()]
+        return json_response(data=res, status=200)
+    return json_response(data={'error' : f'Request type {request.method} not accepted'}, status=400)
 
 if __name__ == "__main__":
     app.run(
